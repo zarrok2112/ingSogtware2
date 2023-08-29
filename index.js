97582c528273b23e2f13183a56406535c5fbad15
zarrok <84244625+zarrok2112@users.noreply.github.com>
2023-08-29 16:12:27 -0500

    aa

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Quiobo people');
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Quiobo People!');
+    });
+
+    app.get('/test/:nombre', (req, res) => {
+        res.send('TESTEO '. req.params.nombre);
+        });
+
+app.listen(port,() => {
+    console.log(`Server running on port ${port}`);
+    } )
